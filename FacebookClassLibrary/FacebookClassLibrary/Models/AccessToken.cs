21e41f8e1596a9d65d346363d852d550054bb881
acheevers <acheevers@AMYC00876>
2018-03-06 16:04:29 +1100

    housekeeping

---
+++ FacebookClassLibrary/FacebookClassLibrary/Models/AccessToken.cs
@@ -0,0 +1,11 @@
+﻿namespace FacebookClassLibrary.Models
+{
+    public class AccessToken
+    {
+        public string Access_Token { get; set; }
+
+        public string Token_Type { get; set; }
+
+        public string Expires_In { get; set; }
+    }
+}
